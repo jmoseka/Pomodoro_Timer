cdb51875cd667fbf4583577728dffb00ab939d5d
jmoseka <jamylam3@gmail.com>
2022-12-10 16:27:58 +0300

    Add css styling

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const updateCountDown = () => {
     var seconds = d.getMinutes() * 60 + d.getSeconds(); //convet current mm:ss to seconds for easier caculation, we don't care hours.
     var fiveMin = 60 * 5; //five minutes is 300 seconds!
     var timeleft = fiveMin - seconds % fiveMin; // let's say now is 01:30, then current seconds is 60+30 = 90. And 90%300 = 90, finally 300-90 = 210. That's the time left!
-    var result = parseInt(timeleft / 60) + ':' + timeleft % 60; //formart seconds back into mm:ss 
+    var result = '0' + parseInt(timeleft / 60) + ':' + timeleft % 60; //formart seconds back into mm:ss 
     timerEl.innerHTML = result;
 }
 
