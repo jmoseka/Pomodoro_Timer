98301df37822117fc4493402c2f5964cb1ac1c1b
jmoseka <jamylam3@gmail.com>
2022-12-11 10:17:18 +0300

    Fix total counts

--- index.js
+++ index.js
@@ -1,21 +1,84 @@
+/* eslint-disable no-plusplus */
+/* eslint-disable no-mixed-operators */
 const timerEl = document.getElementById('time');
 const btnPlay = document.getElementById('play');
 const btnPause = document.getElementById('pause');
 const btnReset = document.getElementById('reset');
+const sessionCountEl = document.getElementById('session-count');
 
+// const startMinutes = 25;
+// const time = startMinutes * 60; /// total seconds in 25minutes
 
-const startMinutes = 25;
-let time = startMinutes * 60; /// total seconds in 25minutes
+btnPlay.addEventListener('click', () => {
+  btnPlay.classList.toggle('active-state');
+  btnPause.classList.remove('active-state');
+  btnReset.classList.remove('active-state');
+});
 
+btnPause.addEventListener('click', () => {
+  btnPause.classList.toggle('active-state');
+  btnPlay.classList.remove('active-state');
+  btnReset.classList.remove('active-state');
+});
 
+btnReset.addEventListener('click', () => {
+  btnReset.classList.toggle('active-state');
+  btnPlay.classList.remove('active-state');
+  btnPause.classList.remove('active-state');
+});
 
+const originalTime = 5;
+let sec = originalTime;
+let secondsEl = 0;
+let minutesEl = 0;
+let sessionCount = 1;
+let totalSession = 0;
+let totalTime = 0;
+let totalHours = 0;
+let totalMinutes = 0;
+
+const updateSessionCount = () => {
+  sessionCount++;
+  if (sessionCount > 4) {
+    // reset session count / 4
+    sessionCount = 1;
+
+    // update total focus time
+    totalTime += originalTime;
+    if (totalTime > 60) {
+      totalHours = parseInt(totalTime / 60, 10);
+      totalMinutes = parseInt(totalTime % 60, 10);
+
+      document.getElementById('total-time').innerHTML = `${totalHours > 1 ? `${totalHours}Hrs` : totalHours} Hr ${totalMinutes} Min`;
+    } else {
+      document.getElementById('total-time').innerHTML = `${totalTime} Minutes`;
+    }
+
+    // update completed session time
+    totalSession++;
+
+    document.getElementById('total-session').innerHTML = totalSession;
+  }
+
+  sessionCountEl.innerHTML = sessionCount;
+};
+
+// update count down function
 const updateCountDown = () => {
-    var d = new Date(); //get current time
-    var seconds = d.getMinutes() * 60 + d.getSeconds(); //convet current mm:ss to seconds for easier caculation, we don't care hours.
-    var fiveMin = 60 * 5; //five minutes is 300 seconds!
-    var timeleft = fiveMin - seconds % fiveMin; // let's say now is 01:30, then current seconds is 60+30 = 90. And 90%300 = 90, finally 300-90 = 210. That's the time left!
-    var result = '0' + parseInt(timeleft / 60) + ':' + timeleft % 60; //formart seconds back into mm:ss 
-    timerEl.innerHTML = result;
-}
+  sec--;
+  secondsEl = sec % 60;
+  minutesEl = parseInt(sec / 60, 10);
 
-setInterval(updateCountDown(), 1000)
+  // display time
+  secondsEl = secondsEl < 10 ? `0${secondsEl}` : secondsEl;
+  minutesEl = minutesEl < 10 ? `0${minutesEl}` : minutesEl;
+  timerEl.innerHTML = `${minutesEl}:${secondsEl}`;
+
+  // check if 25 minutes is over
+  if (minutesEl === '00' && secondsEl === '00') {
+    updateSessionCount();
+    sec = originalTime;
+  }
+};
+
+setInterval(updateCountDown, 1000);
