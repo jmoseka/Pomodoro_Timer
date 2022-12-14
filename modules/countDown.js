17be949a3b1362763acb313ac8853d0a96768694
jmoseka <jamylam3@gmail.com>
2022-12-14 21:48:44 +0300

    add function

---
+++ modules/countDown.js
@@ -0,0 +1,9 @@
+const countDown = (sec) => {
+    if (sec === 25) {
+        updateTimer();
+    }
+
+    if (sec === 5) {
+        updateTimer();
+    }
+}
--- modules/timer.js
+++ modules/timer.js
@@ -1,57 +1,57 @@
+
 /* eslint-disable no-plusplus */
 const timerEl = document.getElementById('time');
 const btnPlay = document.getElementById('play');
 const btnPause = document.getElementById('pause');
 const btnReset = document.getElementById('reset');
 const sessionCountEl = document.getElementById('session-count');
+const completedSessionEl = document.getElementById('total-session');
 
 const originalTime = 25;
+const minuteBreakTime = 10;
 let sec = originalTime;
-let secondsEl = 0;
-let minutesEl = 0;
 let sessionCount = 1;
-let totalSession = 0;
-let totalTime = 0;
-let totalHours = 0;
-let totalMinutes = 0;
+let sessionCompleted = 0;
+let totalFocusTime = 0;
+let id = 0;
 
 let isPaused = true;
+let isTakingBreak = false;
 
+const updateTotalFocusTime = () => {
+  let focusHours = 0;
+  let focusMinutes = 0;
 
-const updateTimeCount = () => {
-    
-}
+  totalFocusTime += originalTime;
+
+  if (totalFocusTime > 60) {
+    focusHours = parseInt(totalFocusTime / 60, 10);
+    focusMinutes = parseInt(totalFocusTime % 60, 10);
+    document.getElementById('total-time').innerHTML = `${focusHours}h ${focusMinutes}m`;
+  } else {
+    document.getElementById('total-time').innerHTML = `${totalFocusTime}m`;
+  }
+};
 
 const updateSessionCount = () => {
 // update the number of sessions in DOM
   sessionCount++;
 
-  if (sessionCount > 1) {
-    // put session count 1 / 4
+  if (sessionCount > 4) {
+    sessionCompleted++;
+    completedSessionEl.innerHTML = sessionCompleted;
+    // reset back to one
     sessionCount = 1;
-
-    // update total focus time
-    totalTime += originalTime;
-    if (totalTime === 60) {
-      totalHours = parseInt(totalTime / 60, 10);
-      totalMinutes = parseInt(totalTime % 60, 10);
-
-      document.getElementById('total-time').innerHTML = `${totalHours > 1 ? `${totalHours} hrs` : `${totalHours} hr`}  ${totalMinutes} min`;
-    } else {
-      document.getElementById('total-time').innerHTML = `${totalTime} Minutes`;
-    }
-
-    // update completed session time
-    totalSession++;
-
-    document.getElementById('total-session').innerHTML = totalSession;
   }
-
-  sessionCountEl.innerHTML = sessionCount;
+  sessionCountEl.innerHTML = `${sessionCount} / 4 sessions`;
 };
 
 // update count down function
+
 const updateCountDown = () => {
+  let secondsEl = 0;
+  let minutesEl = 0;
+
   if (!isPaused) {
     secondsEl = sec % 60; // convert to seconds
     minutesEl = parseInt(sec / 60, 10); // convert to minutes
@@ -63,13 +63,27 @@ const updateCountDown = () => {
 
     // check if 25 minutes is over
     if (minutesEl === '00' && secondsEl === '00') {
-      updateSessionCount();
-      updateTimeCount();
-      // reset to original time
-      sec = originalTime;
+      if (isTakingBreak !== true) {
+        updateTotalFocusTime();
+        updateSessionCount();
+      }
+
+      // take a break
+      // change original time to 10s
+      // take a break
+      // set sec to 5min
+
+      // i
+      isTakingBreak = true;
+      sec = 10;
     }
     sec--;
   }
+};
+
+const pomoTimer = () => {
+  id = setInterval(updateCountDown, 50);
+  console.log(id);
 };
 
 btnPlay.addEventListener('click', () => {
@@ -97,4 +111,4 @@ btnReset.addEventListener('click', () => {
   isPaused = false;
 });
 
-export default updateCountDown;
+export default pomoTimer;
