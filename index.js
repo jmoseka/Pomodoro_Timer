4e38f3586420c3d4357f53073b4121d0d327a606
jmoseka <jamylam3@gmail.com>
2022-12-26 21:30:37 +0300

    add background music styling

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 /* eslint-disable no-plusplus */
 import openCloseNav from './modules/open-close-nav.js';
 import pomoTimer from './modules/timer.js';
+import musicOperation from './modules/music.js';
 
 openCloseNav();
 pomoTimer();
+musicOperation();
---
+++ modules/music.js
@@ -0,0 +1,89 @@
+/* eslint-disable no-plusplus */
+const playBtn = document.querySelector('#play-music');
+const prevBtn = document.querySelector('#prev-music');
+const nextBtn = document.querySelector('#next-music');
+const title = document.querySelector('#music-title');
+const progressContainer = document.querySelector('.progress-container');
+const progress = document.querySelector('.progress');
+const musicContainer = document.querySelector('.music-container');
+const audio = document.querySelector('#audio');
+const songs = ['chill', 'upbeat', 'empty-mind'];
+
+const musicOperation = () => {
+  let songIndex = 2;
+
+  // initially load song
+
+  const loadSong = (song) => {
+    title.textContent = song;
+    audio.src = `music/${song}.mp3`;
+  };
+
+  const playSong = () => {
+    musicContainer.classList.toggle('playing-music');
+    playBtn.querySelector('.icon-play').classList.toggle('fa-play');
+    playBtn.querySelector('.icon-play').classList.toggle('fa-pause');
+    audio.play();
+  };
+
+  const pauseSong = () => {
+    musicContainer.classList.toggle('playing-music');
+    playBtn.querySelector('.icon-play').classList.add('fa-play');
+    playBtn.querySelector('.icon-play').classList.remove('fa-pause');
+    audio.pause();
+  };
+
+  const prevSong = () => {
+    songIndex--;
+    if (songIndex < 0) {
+      songIndex = songs.length - 1;
+    }
+
+    loadSong(songs[songIndex]);
+    playSong();
+  };
+
+  const nextSong = () => {
+    songIndex++;
+    if (songIndex > songs.length - 1) {
+      songIndex = 0;
+    }
+
+    loadSong(songs[songIndex]);
+    playSong();
+  };
+
+  const updateProgress = (e) => {
+    const { duration, currentTime } = e.srcElement;
+    const progressPercent = (currentTime / duration) * 100;
+    progress.style.width = `${progressPercent}%`;
+  };
+
+  // event listeners
+  playBtn.addEventListener(('click'), () => {
+    const isPlaying = musicContainer.classList.contains('playing-music');
+    if (isPlaying) {
+      playSong();
+    } else {
+      pauseSong();
+    }
+  });
+
+  const setProgress = (e) => {
+    const width = this.clientWidth;
+    const clickX = e.offsetX;
+    const { duration } = audio;
+
+    audio.currentTime = (clickX / width) * duration;
+  };
+  prevBtn.addEventListener(('click'), prevSong);
+  nextBtn.addEventListener(('click'), nextSong);
+  audio.addEventListener(('timeupdate'), updateProgress);
+  progressContainer.addEventListener(('click'), setProgress);
+
+
+  audio.addEventListener('ended', nextSong);
+
+  loadSong(songs[songIndex]);
+};
+export default musicOperation;
