7410bb831f373c2f7198391fe67b9d49f41c64f8
OVECJOE <ovecjoe123@gmail.com>
2023-09-16 14:31:44 +0100

    Converted Pomodoro Timer to a PWA with support for caching some of the images to support partially offline access

--- index.js
+++ index.js
@@ -5,3 +5,13 @@ import populateMusicCard from './modules/populateMusicCard.js';
 openCloseNav();
 populateMusicCard();
 pomoTimer();
+
+// Add PWA functionality
+if ('serviceWorker' in navigator) {
+  const result = await navigator.serviceWorker.register('/service_worker.js');
+
+  if (result.active) {
+    // eslint-disable-next-line no-console
+    console.log('Service worker registered!');
+  }
+}
